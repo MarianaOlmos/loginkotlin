a37b15faee39f44c7b6307774ade8407b113cae5
mariana <marianaperezolmos@live.com>
2019-09-29 01:05:29 -0500

    Initial commit

---
+++ app/src/main/java/olmos/mariana/loginkotlin/SegundaActivity.kt
@@ -0,0 +1,12 @@
+package olmos.mariana.loginkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda)
+    }
+}
